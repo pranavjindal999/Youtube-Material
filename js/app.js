199d40f043877c6762db056207e76c642bf2c7d5
Pranav Jindal <pranavjindal999@outlook.com>
2016-06-28 23:45:26 +0530

    used gapi javascript client for api requests instead of $http manual requests

--- js/app.js
+++ js/app.js
@@ -1,4 +1,15 @@
-var app = angular.module('youtube', ['imgLoader','spinner','ui.router']);
 var browserKey = 'AIzaSyDx5tEfNkixUSvbt9PQReDDJLZI16P4Fmk';
-var apiURL = 'https://www.googleapis.com/youtube/v3/';
+var youtubeApi = null;
 
+// initializing google api for youtube..
+
+function init() {
+    gapi.client.setApiKey(browserKey);
+    youtubeApi = gapi.client.load('youtube', 'v3');
+ 	// bootstraping angular after gapi is loaded..
+    angular.element(document).ready(function() {
+            angular.bootstrap(document, ['youtube']);
+        });
+}
+
+var app = angular.module('youtube', ['imgLoader', 'spinner', 'ui.router']);
--- js/services/searchService.js
+++ js/services/searchService.js
@@ -1,23 +1,20 @@
-app.factory('searchService', function($http, $q) {
+app.factory('searchService', function($q) {
     var service = {};
     service.getVideos = function(pageToken, query) {
-    	var data = $q.defer();
-        var url = null;
-        if (pageToken) {
-            url = apiURL + 'search?part=snippet&type=video&maxResults=12&q=' + query + '&pageToken=' + pageToken + '&key=' + browserKey;
-        } else {
-            url = apiURL + 'search?part=snippet&type=video&maxResults=12&q=' + query + '&key=' + browserKey;
-        }
 
-        $http({
-            method: 'GET',
-            url: url
-        }).then(function successCallback(response) {
-        	data.resolve(response.data);
-        }, null);
+        var data = $q.defer();
+        youtubeApi.then(function() {
+            gapi.client.youtube.search.list({
+                'part': 'snippet',
+                'type': 'video',
+                'maxResults': '12',
+                'q': query,
+                'pageToken': pageToken
+            }).then(function(response) {
+                data.resolve(response.result);
+            });
+        })
         return data.promise;
     }
-
-
     return service;
 });
