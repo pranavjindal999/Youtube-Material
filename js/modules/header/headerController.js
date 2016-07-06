1233de820e7841d7ae50992c3b3eaf0ab5423408
Pranav Jindal <pranavjindal999@outlook.com>
2016-07-07 00:54:33 +0530

    search suggestion, major changes

--- js/modules/header/headerController.js
+++ js/modules/header/headerController.js
@@ -1,14 +1,44 @@
 (function(angular) {
-    app.controller('headerController', ['$scope', '$state', '$location',
-        function($scope, $state, $location) {
+    app.controller('headerController', ['$scope', '$state', '$location', 'searchService',
+        function($scope, $state, $location, searchService) {
 
-            if ($location.search().query) {
-                $scope.query = $location.search().query;
+            $scope.fetchSearchSuggestion = function() {
+                searchService.getSearchSuggestion($scope.query)
+                    .then(function(data) {
+                        $scope.items = data;
+                    })
+            }
+            $scope.suggestionClick = function(query) {
+                $scope.query = query;
+                $scope.submit();
+            }
+            $scope.items = ["tvf", "tvf play", "tvf pitchers", "tvf permanent roommates"];
+            $scope.searchFocused = function() {
+                $scope.search_ul = "nav-items-focus right";
+                $scope.search_li = "search-item-li-focus";
+                $scope.mobileSearchFocus = "hide-on-small-only";
+                $scope.crossSign = true;
+                $scope.searchSuggestion = true;
+            }
+
+            $scope.searchBlured = function() {
+                $scope.search_ul = "right nav-items";
+                $scope.search_li = "";
+                $scope.mobileSearchFocus = "";
+                $scope.crossSign = false;
+                $scope.searchSuggestion = false;
             }
 
             $scope.submit = function($event) {
-                $event.target.blur();
+                try { $event.target.blur(); } catch (err) {}
                 $state.go('home.searchVideos', { query: $scope.query, pageToken: null });
+            }
+
+            $scope.init = function() {
+                if ($location.search().query) {
+                    $scope.query = $location.search().query;
+                }
+                $scope.searchBlured();
             }
 
         }
--- js/services/searchService.js
+++ js/services/searchService.js
@@ -1,7 +1,6 @@
-app.factory('searchService', function($q) {
+app.factory('searchService', function($q, $http) {
     var service = {};
     service.getVideos = function(pageToken, query) {
-
         var data = $q.defer();
         youtubeApi.then(function() {
             gapi.client.youtube.search.list({
@@ -15,6 +14,23 @@ app.factory('searchService', function($q) {
             });
         })
         return data.promise;
+    }
+
+    service.getSearchSuggestion = function(query) {
+        var q = $q.defer();
+        $http({
+            method: 'JSONP',
+            headers: {
+                'Access-Control-Allow-Origin': '*',
+                'Access-Control-Allow-Methods': 'POST, GET, OPTIONS, PUT',
+                'Content-Type': 'application/json',
+                'Accept': 'application/json'
+            },
+            url: 'http://suggestqueries.google.com/complete/search?callback=JSON_CALLBACK&ds=yt&client=firefox&hl=en&q=' + query
+        }).success(function(data) {
+            q.resolve(data);
+        })
+        return q.promise;
     }
     return service;
 });
