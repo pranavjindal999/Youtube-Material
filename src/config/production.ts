42c421ca344905c2a338aac6836b43d35a4e8f99
Pranav Jindal <pranavjindal999@outlook.com>
2018-10-13 12:46:16 +0530

    update GA keys

--- src/config/production.ts
+++ src/config/production.ts
@@ -4,7 +4,7 @@ let production: Partial<IConfig> = {
   apiKey: "AIzaSyDsFPS9ck8-klIo2lq5iRnixVNCO2DnLvs",
   debugInfo: false,
   xhrTimeout: 1000 * 60,
-  gaId: "UA-81891151-1"
+  gaId: "UA-81891151-3"
 };
 
 export default production;
--- src/config/staging.ts
+++ src/config/staging.ts
@@ -4,7 +4,7 @@ let staging: Partial<IConfig> = {
   apiKey: "AIzaSyDIeWrGesmBHipi26KQFldltK51zjw1R8Y",
   debugInfo: false,
   xhrTimeout: 1000 * 60,
-  gaId: "UA-81891151-2"
+  gaId: "UA-81891151-4"
 };
 
 export default staging;
